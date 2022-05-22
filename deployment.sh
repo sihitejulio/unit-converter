1da2e46e9bd3f3ba6777895a29c56d9414ce67b9
julio p sihite <julio.sihite@finaccel.co>
2022-05-22 11:32:31 +0700

    update deployment

--- deployment.sh
+++ deployment.sh
@@ -2,7 +2,7 @@
 # Build docker image & run
 function build_and_run {
     docker build -t frontend_bank:v1 .
-    docker run --name frontend_bank -d -p 8000:80 frontend_bank:v1
+    docker run --name frontend_bank -d -p 8001:3000 frontend_bank:v1
 }
 
 build_and_run
